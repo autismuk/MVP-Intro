6f4fc67f6f42cb0995bfdfec8f1544dd1a609ac5
Paul Robson <paul@robsons.org.uk>
2014-04-25 07:12:42 +0100

    oops, odd stuff

--- Part1/main.lua
+++ Part1/main.lua
@@ -8,9 +8,6 @@ local model = require("model") 														-- pull in the model and the view.
 local view = require("view")
 
 model.initialise() 																	-- initialise the model and the view
-
-
-print(model.bat.x)
 view.initialise()
 
 view.update(model) 																	-- update the view from the model.
